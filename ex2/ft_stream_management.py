27a85e19a27d52444e0c992653b16ee73911a0bb
若月　洸 <hikaru@wakatsukikounoMacBook-Air.local>
2026-02-08 16:03:26 +0900

    wak

--- ex2/ft_stream_management.py
+++ ex2/ft_stream_management.py
@@ -3,14 +3,18 @@ import sys
 
 def ft_stream_management() -> None:
     print("=== CYBER ARCHIVES - COMMUNICATION SYSTEM ===")
-    archivist_id: str = input("Input Stream active. Enter archivist ID: ")
-    status_report: str = input("Input Stream active. Enter status report: ")
-    print()
-    sys.stdout.write(f"[STANDARD] Archive status from "
-                     f"{archivist_id}: {status_report}\n")
-    sys.stderr.write("[ALERT] System diagnostic: "
-                     "Communication channels verified\n")
-    sys.stdout.write("[STANDARD] Data transmission complete\n")
+    try:
+        archivist_id: str = input("Input Stream active. Enter archivist ID: ")
+        status_report: str = input("Input Stream active. Enter status report: ")
+        print()
+        sys.stdout.write(f"[STANDARD] Archive status from "
+                         f"{archivist_id}: {status_report}\n")
+        sys.stderr.write("[ALERT] System diagnostic: "
+                         "Communication channels verified\n")
+        sys.stdout.write("[STANDARD] Data transmission complete\n")
+    except Exception as e:
+        print(e)
+        return
     print()
     print("Three-channel communication test successful.")
 
